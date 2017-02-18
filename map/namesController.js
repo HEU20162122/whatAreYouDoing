a3fc3583ca3b0bbf9cd6cbad11d5720b61937804
Smityz <527646889@qq.com>
2017-02-18 20:33:25 +0800

    增加了一个未完成功能，大家一起想点，然后一起解决

---
+++ map/namesController.js
@@ -0,0 +1,121 @@
+
+myapp1 = angular.module('myApp', []);
+myapp1.controller('wyCtrl', function ($scope) {
+      $scope.items = [
+          {title:'我想要个女朋友啊啊啊啊啊啊啊啊',time:'17.02.14'},
+          {title:'说实话不一定要女的- -',time:'17.02.29'},
+          {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+      ];
+    });
+myapp1.controller('rwCtrl', function($scope) {
+      $scope.items = [
+          {title:'我爱中文系妹子',time:'17.02.14'},
+          {title:'萌萌哒- -',time:'17.02.29'},
+          {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+      ];
+});
+myapp1.controller('cbCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱船舶系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('hjCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱航建系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('ndCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('ssCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱水声系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('zdhCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('jdCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('csCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱cs系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('jgCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('xtCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('lCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('chCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('hCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('tyCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('gfCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
+myapp1.controller('ghCtrl', function($scope) {
+    $scope.items = [
+        {title:'我爱中文系妹子',time:'17.02.14'},
+        {title:'萌萌哒- -',time:'17.02.29'},
+        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
+    ];
+});
