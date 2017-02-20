eca04f2b9c585c671d11b0f409f7812d017a5ce2
Smityz <527646889@qq.com>
2017-02-20 17:07:32 +0800

    修改了文档的BUG，改动了第四层，控制器里改成了HTTP通信样式

--- map/namesController.js
+++ map/namesController.js
@@ -1,121 +1,102 @@
 
 myapp1 = angular.module('myApp', []);
-myapp1.controller('wyCtrl', function ($scope) {
-      $scope.items = [
-          {title:'我想要个女朋友啊啊啊啊啊啊啊啊',time:'17.02.14'},
-          {title:'说实话不一定要女的- -',time:'17.02.29'},
-          {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-      ];
-    });
-myapp1.controller('rwCtrl', function($scope) {
-      $scope.items = [
-          {title:'我爱中文系妹子',time:'17.02.14'},
-          {title:'萌萌哒- -',time:'17.02.29'},
-          {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-      ];
+myapp1.controller('wyCtrl', function ($scope, $http) {
+    $http.get("/php/wy.php")
+    .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('cbCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱船舶系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('rwCtrl', function($scope, $http) {
+    $http.get("/php/rw.php")
+    .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('hjCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱航建系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('cbCtrl', function($scope, $http) {
+    $http.get("/php/cb.php")
+    .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('ndCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('hjCtrl', function($scope, $http) {
+    $http.get("/php/hj.php")
+    .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('ssCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱水声系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('ndCtrl', function($scope, $http) {
+    $http.get("/php/nd.php")
+    .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('zdhCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('ssCtrl', function($scope, $http) {
+    $http.get("/php/ss.php")
+    .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('jdCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('zdhCtrl', function($scope, $http) {
+  $http.get("/php/zdh.php")
+      .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('csCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱cs系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('jdCtrl', function($scope, $http) {
+  $http.get("/php/jd.php")
+  .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('jgCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('csCtrl', function($scope, $http) {
+  $http.get("/php/cs.php")
+  .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('xtCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('jgCtrl', function($scope, $http) {
+  $http.get("/php/jg.php")
+  .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('lCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('xtCtrl', function($scope, $http) {
+  $http.get("/php/wy.php")
+      .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('chCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('lCtrl', function($scope, $http) {
+  $http.get("/php/wy.php")
+  .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('hCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('chCtrl', function($scope, $http) {
+  $http.get("/php/wy.php")
+      .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('tyCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('hCtrl', function($scope, $http) {
+  $http.get("/php/wy.php")
+      .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('gfCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('tyCtrl', function($scope, $http) {
+  $http.get("/php/wy.php")
+      .success(function (response) {$scope.items = response.records;});
 });
-myapp1.controller('ghCtrl', function($scope) {
-    $scope.items = [
-        {title:'我爱中文系妹子',time:'17.02.14'},
-        {title:'萌萌哒- -',time:'17.02.29'},
-        {title:'男的也可以试一试哦呵呵呵呵呵呵呵呵O(∩_∩)O哈哈~',time:'17.2.33'}
-    ];
+myapp1.controller('gfCtrl', function($scope, $http) {
+  $http.get("/php/wy.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('ghCtrl', function($scope, $http) {
+  $http.get("/php/wy.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('xshCtrl', function($scope, $http) {
+  $http.get("/php/xsh.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('yjshCtrl', function($scope, $http) {
+  $http.get("/php/yjsh.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('slCtrl', function($scope, $http) {
+  $http.get("/php/sl.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('kxCtrl', function($scope, $http) {
+  $http.get("/php/kx.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('dhdcCtrl', function($scope, $http) {
+  $http.get("/php/dhdc.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('zzsCtrl', function($scope, $http) {
+  $http.get("/php/zzs.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('ygltCtrl', function($scope, $http) {
+  $http.get("/php/yglt.php")
+      .success(function (response) {$scope.items = response.records;});
+});
+myapp1.controller('dcCtrl', function($scope, $http) {
+  $http.get("/php/dc.php")
+      .success(function (response) {$scope.items = response.records;});
 });
