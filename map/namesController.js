d7afc1b6ed670d029236c837b621423572f62daa
Smityz <527646889@qq.com>
2017-03-01 00:22:32 +0800

    修改了通信部分写法，实验一下

--- map/namesController.js
+++ map/namesController.js
@@ -1,102 +1,363 @@
 
 myapp1 = angular.module('myApp', []);
-myapp1.controller('wyCtrl', function ($scope, $http) {
-    $http.get("/php/wy.php")
-    .success(function (response) {$scope.items = response.records;});
+  myapp1.controller('wyCtrl', function ($scope, $http) {
+  $http.get('php/search.php', {
+    params: {
+        type: 'college',
+        element: 'wy',
+    }
+  }).then(function (result) {  //正确请求成功时处理
+    console.info(result);
+    alert(result.data);
+  }).catch(function (result) { //捕捉错误处理
+    console.info(result);
+    alert(result.data.Message);
+  });
 });
+
 myapp1.controller('rwCtrl', function($scope, $http) {
-    $http.get("/php/rw.php")
-    .success(function (response) {$scope.items = response.records;});
+    $http.get('php/search.php', {
+    params: {
+      type: 'college',
+        element: 'rw',
+    }
+  }).then(function (result) {  //正确请求成功时处理
+    console.info(result);
+    alert(result.data);
+  }).catch(function (result) { //捕捉错误处理
+    console.info(result);
+    alert(result.data.Message);
+  });
 });
+
 myapp1.controller('cbCtrl', function($scope, $http) {
-    $http.get("/php/cb.php")
-    .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'cb',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('hjCtrl', function($scope, $http) {
-    $http.get("/php/hj.php")
-    .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'hj',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('ndCtrl', function($scope, $http) {
-    $http.get("/php/nd.php")
-    .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'nd',
+      type: 'college',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('ssCtrl', function($scope, $http) {
-    $http.get("/php/ss.php")
-    .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'ss',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('zdhCtrl', function($scope, $http) {
-  $http.get("/php/zdh.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'zdh',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('jdCtrl', function($scope, $http) {
-  $http.get("/php/jd.php")
-  .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'jd',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('csCtrl', function($scope, $http) {
-  $http.get("/php/cs.php")
-  .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'cs',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('jgCtrl', function($scope, $http) {
-  $http.get("/php/jg.php")
-  .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'jg',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('xtCtrl', function($scope, $http) {
-  $http.get("/php/wy.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'xt',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('lCtrl', function($scope, $http) {
-  $http.get("/php/wy.php")
-  .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+    type: 'college',
+      element: 'l',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('chCtrl', function($scope, $http) {
-  $http.get("/php/wy.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'ch',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('hCtrl', function($scope, $http) {
-  $http.get("/php/wy.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'h',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('tyCtrl', function($scope, $http) {
-  $http.get("/php/wy.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'ty',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('gfCtrl', function($scope, $http) {
-  $http.get("/php/wy.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'gf',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('ghCtrl', function($scope, $http) {
-  $http.get("/php/wy.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'gh',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('xshCtrl', function($scope, $http) {
-  $http.get("/php/xsh.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'xsh',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('yjshCtrl', function($scope, $http) {
-  $http.get("/php/yjsh.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'yjsh',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('slCtrl', function($scope, $http) {
-  $http.get("/php/sl.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'sl',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('kxCtrl', function($scope, $http) {
-  $http.get("/php/kx.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'kx',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('dhdcCtrl', function($scope, $http) {
-  $http.get("/php/dhdc.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'dhdc',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('zzsCtrl', function($scope, $http) {
-  $http.get("/php/zzs.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'zzs',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('ygltCtrl', function($scope, $http) {
-  $http.get("/php/yglt.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'yglt',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
 });
+});
+
 myapp1.controller('dcCtrl', function($scope, $http) {
-  $http.get("/php/dc.php")
-      .success(function (response) {$scope.items = response.records;});
+  $http.get('php/search.php', {
+  params: {
+      element: 'dc',
+  }
+}).then(function (result) {  //正确请求成功时处理
+  console.info(result);
+  alert(result.data);
+}).catch(function (result) { //捕捉错误处理
+  console.info(result);
+  alert(result.data.Message);
+});
 });
