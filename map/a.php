407e4f43d3826cf8c12c2cc2066c3d4c23af666b
Smityz <527646889@qq.com>
2017-03-06 22:54:33 +0800

    1

--- map/a.php
+++ map/a.php
@@ -1,3 +1,22 @@
 <?php
-echo $_GET['type']+$_GET['element'];
+class Item{
+  var $tit;
+  var $age;
+}
+
+
+$type = urldecode($_GET['type']);
+$element = urldecode($_GET['element']);
+$item1 = new Item();
+$item1->tit = "123";
+$item1->time = "345";
+
+
+$item2 = new Item();
+$item2->tit = "ert";
+$item2->time = "qwe";
+
+$data = array( item1,item2);
+header('Content-type: application/json');
+echo json_encode($data);
  ?>
--- map/namesController.js
+++ map/namesController.js
@@ -5,9 +5,23 @@ $http.get('a.php',{
   params:{type: 1,
     element: 2},
 }).then(function(res){
+  console.log(res);
+  var item = res.data;
+  var str = JSON.stringify(item);
+  $scope.items=res.data;
+  },function(){
+    alert('fault');
+  });
+});
+
+myapp1.controller('jdCtrl', function($scope,$http) {
+$http.get('a.php',{
+  params:{type: 1,
+    element: 2},
+}).then(function(res){
   alert('yes');
   $scope.items=res.data;
   },function(){
-    alert('falut');
+    alert('fault');
   });
 });
