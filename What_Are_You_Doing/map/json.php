5cf56a54155298b0149d3b1e43fe3eef6a84c302
cuteluckydog <1571087769@qq.com>
2017-03-10 13:30:41 +0800

    移出矢量图到img，json.php适合php 7

--- What_Are_You_Doing/map/json.php
+++ What_Are_You_Doing/map/json.php
@@ -1,39 +1,20 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
 <?php
+	$mysqli = new mysqli("138.68.12.160", "cuteluckydog", "Xhsca312", "whatAreYouDoing");
+	echo 12;
+	
+	$sql = "SELECT * FROM try";
+	$result = $mysqli->query($sql);
 
-	$link = mysql_connect( 'localhost','root','Xhsca312' );
-		if(!$link){
-			die('连接失败；<br />错误原因：'.mysql_error() );
-		}
-	
-	
-	mysql_select_db( 'book_store',$link )or die('无法选中数据库；<br />错误原因：'.mysql_error() );
-	
-	
-	$result = mysql_query( "SELECT * FROM books");
-	
-	
 
-	while($row = mysql_fetch_assoc($result) ){
-		$results[] = $row;
+	while($row = $result->fetch_assoc() ){
+		$results[]=$row;
 	};
- 	
 	$json = json_encode($results);
-	
+
 	echo $json;
-	
 
-	mysql_free_result( $result );
-	
-	mysql_close($link);
+
+	$result->free();
+
+	$mysqli->close();
 ?>
-
-</body>
-</html>
