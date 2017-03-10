3418ff130be1dbe59b55de58a8fe6cd666f3180f
Smityz <527646889@qq.com>
2017-03-11 01:21:02 +0800

    稳定

--- What_Are_You_Doing/map/namesController.js
+++ What_Are_You_Doing/map/namesController.js
@@ -2,8 +2,8 @@ myapp1 = angular.module('myApp', []);
 
 myapp1.controller('jgCtrl', function($scope,$http) {
 $http.get('json.php',{
-    params:{type: 1,
-      element: 2},
+    params:{type: '2',
+      },
 }).then(function(res){
   $scope.items=res.data;
   console.log(res);
