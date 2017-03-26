eb5c5b2893ed37d504ee03d0f45f3d7d5ff4559f
haoshuting <1134744980@qq.com>
2017-03-26 22:52:09 +0800

    hst

---
+++ green/namesController.js
@@ -0,0 +1,50 @@
+angular.module('myApp', []).
+controller('namesCtrl', function($scope)
+{
+  $scope.name = "匠心 * 有朋";
+  $scope.name1 = "校团委";
+  $scope.name2 = "3月23号我们等你";
+  $scope.name3 = "大明湖畔的五阿哥来工程啦";
+  $scope.name4 = "五阿哥苏有朋将于3月23日来工程演讲你们是不是也和我一样激动，有木有？！";
+  $scope.name41 = "我们的五阿哥苏有朋不仅仅会演戏会唱歌";
+  $scope.name42 = "这次来做客还带来了他导演的悬疑电影————嫌疑人X的献身！"
+  $scope.name5 = "快点击我看看吧";
+  $scope.name6 = "科普五阿哥啦";
+  $scope.name7 = "敲黑板画重点";
+  $scope.name8 = "很多人都不知道，咱们集天地日月精华汇聚无数灵气的五阿哥";
+  $scope.name81 = "曾以全台第五的优异成绩考进——台湾大学工程系";
+  $scope.name82 = "是一位名副其实的学霸大咖";
+  $scope.name9 = "images/pic01.jpg";
+  $scope.name10 = "images/pic02.jpg";
+  $scope.name11 = "Mollis posuere";
+  $scope.name111 = "wejgdk hedhewi";
+  $scope.name12 = "Rhoncus mattis egestas sed fusce sodales";
+  $scope.name121 = "rutrum et etiam ullamcorper. Etiam egestas scelerisque";
+  $scope.name122= "ac duis magna lorem ipsum dolor.";
+  $scope.name13 = "images/pic02.jpg";
+  $scope.name14 = "sewax cewvv";
+  $scope.name141 = "lectus lacus"
+  $scope.name15 = "Rhoncus mattis egestas sed fusce sodales ";
+  $scope.name151 = "rutrum et etiam ullamcorper.";
+  $scope.name152 = "Etiam egestas scelerisque ac duis magna lorem ipsum dolor.";
+  $scope.name16 = "Magnis parturient";
+  $scope.name17 = "Justo phasellus et aenean dignissim";
+  $scope.name171 = "placerat cubilia purus lectus.";
+  $scope.name181 = "images/fulls/01.jpg";
+  $scope.name182 = "images/pic02.jpg";
+  $scope.name182 = "Mollis posuere";
+  $scope.name = "匠心 * 有朋";
+  $scope.name1 = "校团委";
+  $scope.name2 = "3月23号我们等你";
+  $scope.name3 = "大明湖畔的五阿哥来工程啦";
+  $scope.name4 = "五阿哥苏有朋将于3月23日来工程演讲你们是不是也和我一样激动，有木有？！";
+  $scope.name41 = "我们的五阿哥苏有朋不仅仅会演戏会唱歌";
+  $scope.name42 = "这次来做客还带来了他导演的悬疑电影————嫌疑人X的献身！"
+  $scope.name5 = "快点击我看看吧";
+  $scope.name6 = "科普五阿哥啦";
+  $scope.name7 = "敲黑板画重点";
+  $scope.name8 = "很多人都不知道，咱们集天地日月精华汇聚无数灵气的五阿哥";
+  $scope.name81 = "曾以全台第五的优异成绩考进——台湾大学工程系";
+  $scope.name82 = "是一位名副其实的学霸大咖";
+}
+);
