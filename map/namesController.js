1fb3b3f2d66dee883d1dff1343cfa482082f1354
Smityz <527646889@qq.com>
2017-03-27 13:44:00 +0800

    3.27

--- map/namesController.js
+++ map/namesController.js
@@ -4,11 +4,11 @@ myapp1.controller('jgCtrl', function($scope,$http) {
 $http({
       method  : 'POST',
       url     : 'json.php',
-      data    : $.param({'jg'}),  // pass in data as strings
+      data    : $.param(type="jg"),  // pass in data as strings
       headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
     })
 $http.get('json.php',{
-      params:{type: 'jg',},
+      params:{type: "jg1",},
 }).then(function(res){
     $scope.items=res.data;
     console.log(res);
