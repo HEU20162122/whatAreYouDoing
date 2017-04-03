3f68442ed1997ba7e282246d8376263d2a057167
cuteluckydog <1571087769@qq.com>
2017-04-03 19:56:40 +0800

    真要死了

---
+++ a.php
@@ -0,0 +1,41 @@
+<?php
+
+
+	 $title=$_POST['title'];
+	 $place=$_POST['place'];
+	 $time=$_POST['time'];
+	 $college=$_POST['college'];
+ 	 $orginization=$_POST['orginization'];
+	 $word1=$_POST['word1'];
+	 $word2=$_POST['word2'];
+	 $word3=$_POST['word3'];
+
+
+
+
+	/* $myfile = fopen("a.txt", "w") or die("Unable to open file!");
+	 fwrite($myfile, $title);
+	 fwrite($myfile, $college);
+	 fwrite($myfile, $word2);
+	 fwrite($myfile, $word3);
+   fwrite($myfile, $word1);
+	 fwrite($myfile, $place);
+	 fwrite($myfile, $time);
+	 fwrite($myfile, $orginization);
+	 fwrite($myfile, $word);
+	 fclose($myfile);
+*/
+
+
+	 $mysqli = new mysqli('localhost', 'root', 'Xhsca312', 'whatAreYouDoing');
+	 /*if(mysqli_connect_error()){
+	     echo mysqli_connect_error();
+	 }else{
+	   echo "yes";
+	 }*/
+	 $sql = "INSERT INTO info (title,place,word1,word2,word3,college,time,orginization) VALUES ('$title','$place','$word1','$word2','$word3','$college','$time','$orginization')";
+	 $result = $mysqli->query($sql);
+
+	 $mysqli->close();
+
+	 ?>
